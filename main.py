6067e2bccf0b29a35e2f8b7aa8c503ee0ce20a83
will-da-pro <william@dolier.net>
2025-05-07 14:33:05 +1000

    added front and spinning motor

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pybricks.pupdevices import Motor, UltrasonicSensor
 from pybricks.robotics import DriveBase
 
 hub = PrimeHub()
-hub.speaker.volume(100)
+hub.speaker.volume(50)
 hub.speaker.beep(duration=100)
 
 left_motor: Motor = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
@@ -21,18 +21,25 @@ base.use_gyro(True)
 base.turn(360)
 base.drive(100, 1)
 
-ultrasonic: UltrasonicSensor = UltrasonicSensor(Port.A)
+spinning_ultrasonic: UltrasonicSensor = UltrasonicSensor(Port.A)
+ultrasonic_motor: Motor = Motor(Port.E)
+
+front_ultrasonic: UltrasonicSensor = UltrasonicSensor(Port.B)
 
 class Robot:
-    def __init__(self, hub: PrimeHub, base: DriveBase, ultrasonic: UltrasonicSensor) -> None:
+    def __init__(self, hub: PrimeHub, base: DriveBase, spinning_ultrasonic: UltrasonicSensor, ultrasonic_motor: Motor, front_ultrasonic: UltrasonicSensor) -> None:
         self.hub: PrimeHub = hub
         self.base: DriveBase = base
-        self.ultrasonic: UltrasonicSensor = ultrasonic
+
+        self.spinning_ultrasonic: UltrasonicSensor = spinning_ultrasonic
+        self.ultrasonic_motor: Motor = ultrasonic_motor
+
+        self.front_ultrasonic: UltrasonicSensor = front_ultrasonic
 
         self.wall_dist: int = 200
 
-    def check_dist(self) -> bool:
-        dist = self.ultrasonic.distance()
+    def check_front_dist(self) -> bool:
+        dist = self.front_ultrasonic.distance()
 
         if dist < self.wall_dist:
             return True
@@ -42,14 +49,14 @@ class Robot:
 
     def loop(self) -> None:
         while True:
-            turn: bool = self.check_dist()
+            wall: bool = self.check_front_dist()
 
-            if turn:
+            if wall:
                 return
 
             self.base.drive(100, 0)
 
-robot = Robot(hub, base, ultrasonic)
+robot = Robot(hub, base, spinning_ultrasonic, ultrasonic_motor, front_ultrasonic)
 robot.loop()
 hub.speaker.beep(duration=1000)
 
