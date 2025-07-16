1587947d9e38089b2b74a3ed78c7e0e3e3026ca8
William D'Olier <william@dolier.net>
2025-07-16 19:36:09 +1000

    small changes

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
-from scipy.odr import ODR, Model, Output, RealData
-import scipy
 import numpy as np
+import scipy.odr as odrpack
+np.random.seed(1)
 
-scipy.test()
+N = 100
+x = np.linspace(0,10,N)
+y = 3*x - 1 + np.random.random(N)
+sx = np.random.random(N)
+sy = np.random.random(N)
 
-x = np.array([1, 2, 3, 4, 5])
-y = np.array([7, 6, 5, 4, 3])
+def f(B, x):
+    return B[0]*x + B[1]
+linear = odrpack.Model(f)
+# mydata = odrpack.Data(x, y, wd=1./np.power(sx,2), we=1./np.power(sy,2))
+mydata = odrpack.RealData(x, y, sx=sx, sy=sy)
 
-def model(p, x) -> float:
-    return p[0] * x * p[1]
-
-model2 = Model(model)
-data = RealData(x, y)
-odr_model = ODR(data, model2, beta0=(0., 0.))
-out = odr_model.run()
-print(out.stopreason)
-print(out.beta)
+myodr = odrpack.ODR(mydata, linear, beta0=[1., 2.])
+myoutput = myodr.run()
+myoutput.pprint()
