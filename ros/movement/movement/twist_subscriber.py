8631ace9ec54421838c1a76da3a35a1134fa6de0
William D'Olier <william@dolier.net>
2025-08-17 11:57:45 +1000

    nav2 still not working, time to abandon this

--- ros/movement/movement/twist_subscriber.py
+++ ros/movement/movement/twist_subscriber.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
 
+import math
 import serial
 
 class TwistSubscriber(Node):
@@ -22,14 +23,33 @@ class TwistSubscriber(Node):
 
         self.ser = serial.Serial("/dev/ttyAMA0", 115200)
 
+        self.wheel_dist = 0.185 # m
+        self.counts_per_revolution = 480
+        self.max_counts_per_second = 900
+        self.max_speed = self.max_counts_per_second / self.counts_per_revolution * 2 * math.pi
+        self.wheel_radius = 0.04
+        self.speed_mult = self.counts_per_revolution / (self.wheel_radius * 2 * math.pi)
+
         self.get_logger().info("Twist subscriber node started!")
 
     def stop(self):
         self.ser.write(self.START_FLAG + self.STOP_REQUEST)
 
     def twist_callback(self, msg):
-        linear_x = msg.linear.x
-        angular_z = msg.angular.z
+        linear_x = (msg.linear.x * self.speed_mult * 127) // self.max_speed
+        angular_z = (msg.angular.z * self.speed_mult * 127) // self.max_speed
+
+        if linear_x > 127:
+            linear_x = 127
+
+        if linear_x < -127:
+            linear_x = -127
+
+        if angular_z > 127:
+            angular_z = 127
+
+        if angular_z < 127:
+            angular_z = -127
 
         self.ser.write(self.START_FLAG + self.DRIVE_REQUEST + int(linear_x).to_bytes(1) + int(angular_z).to_bytes(1))
 
--- run-docker.sh
+++ run-docker.sh
@@ -3,8 +3,6 @@ docker run -it \
 --net=host \
 -v /dev:/dev/ \
 -v /run/udev/:/run/udev/ \
--v /tmp/.X11-unix:/tmp/.X11-unix \
 --group-add video \
 -e HOME=/tpm \
--e DISPLAY=$DISPLAY \
 maze/ros
